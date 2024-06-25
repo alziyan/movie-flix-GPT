0345f98c2624e2d76b677e4ace49341f9657930e
alziyan <ansari.alziyan@gmail.com>
2024-06-26 03:38:09 +0530

    Final

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+// import openai from "../utils/openai";
+import OpenAI from "openai";
+import { OPENAI_KEY } from "./../utils/constants";
 
 const GptSearchBar = () => {
+  const searchText = useRef(null);
+  const openai = new OpenAI({
+    apiKey: OPENAI_KEY, // This is the default and can be omitted
+    dangerouslyAllowBrowser: true,
+  });
+
+  const handleGptSearchClick = async () => {
+    console.log(searchText.current.value);
+    // MAKE AN API CALL TO GET MOVIE RESULT
+
+    const gptQuery =
+      "Act as a Movie Recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". only give me names of 2 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholey, Pathan, Golmaal, Koi Mil Gaya";
+
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: gptQuery }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(chatCompletion.choices);
+    // console.log(gptResults.choices?.[0]?.message?.content);
+  };
+
   return (
     <div className="pt-[10%] flex justify-center rounded-full">
       <form
@@ -8,11 +35,15 @@ const GptSearchBar = () => {
         onSubmit={(e) => e.preventDefault()}
       >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 bg-black text-xl rounded-lg bg-opacity-10   text-white col-span-9"
           placeholder="what would you like to watch today ?"
         />
-        <button className="p-4 m-4 text-xl  bg-red-700  rounded-lg text-white col-span-3 ">
+        <button
+          className="p-4 m-4 text-xl  bg-red-700  rounded-lg text-white col-span-3 "
+          // onClick={handleGptSearchClick}
+        >
           Search
         </button>
       </form>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,3 +14,5 @@ export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 
 export const IMG_BG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/51c1d7f7-3179-4a55-93d9-704722898999/be90e543-c951-40d0-9ef5-e067f3e33d16/IN-en-20240610-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const OPENAI_KEY = "OPEN.AI API IS EXPENSIVE";
